04acc0adf3f56542ef2915d442254ef2a5e121f4
Paskalis Abhista Bagaskara Yustiyanto <70842764+paskalisabhista@users.noreply.github.com>
2021-09-26 16:58:54 +0700

    OS212 cbkadal TXT

--- TXT/myscript.sh
+++ TXT/myscript.sh
@@ -13,7 +13,7 @@
 # You new to set "REC2" with your own Public-Key Identity!
 # Check it out with "gpg --list-key"
 
-REC2="cbkadal@localhost"
+REC2="paskalis.abhista@ui.ac.id"
 REC1="operatingsystems@vlsm.org"
 FILES="my*.asc my*.txt my*.sh"
 SHA="SHA256SUM"
@@ -51,3 +51,5 @@ gpg --output $SHA.asc --armor --sign --detach-sign $SHA
 
 echo "gpg --verify $SHA.asc $SHA"
 gpg --verify $SHA.asc $SHA
+
+exit 0
